4244444363d11be3ac9fca1e5db36b58ba22cd6f
pranaya_pradhan <saiju.pranu@gmail.com>
2025-04-18 23:26:42 +0545

    Project Complete

---
+++ admin/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Navbar} from './Components/Navbar/Navbar.jsx'
+import { Admin } from './Pages/Admin/Admin.jsx'
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <Admin/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+export const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Review (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>An e-commerce website is an online platform where businesses and individuals buy and sell products or services over the internet. These websites provide a digital storefront that allows customers to browse products, add them to a shopping cart, and complete transactions using secure payment gateways. E-commerce websites can operate under different models, such as B2C (Business-to-Consumer), B2B (Business-to-Business), C2C (Consumer-to-Consumer), and C2B (Consumer-to-Business).</p>
+            <p>They typically include essential features like product listings, order management, payment processing, customer accounts, and shipping options. Popular examples of e-commerce websites include Amazon, eBay, Alibaba, and Shopify. The rise of e-commerce has transformed the way people shop, offering convenience, a wide range of products, and global accessibility.</p>
+            
+        </div>
+    </div>
+  )
+}
